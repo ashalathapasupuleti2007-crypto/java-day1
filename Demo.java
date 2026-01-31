4b3e14022a64838b98177c0af04ebe4d35dd01c4
ashalathapasupuleti2007-crypto <ashalathapasupuleti2007@gmail.com>
2026-01-31 14:30:57 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,3 +1,8 @@
-public class Demo {
-    
+class Demo{
+    public static void main(String[] args) {
+        int a=20;
+        System.out.println("hello");
+        System.out.println(a);
+        System.out.print("good morning");
+    }
 }
