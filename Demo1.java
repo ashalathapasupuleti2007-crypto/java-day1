acc253e609cb90e0e98fdbac6211c4a2f0d75af4
ashalathapasupuleti2007-crypto <ashalathapasupuleti2007@gmail.com>
2026-01-31 15:47:03 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Demo1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        double a=sc.nextDouble();
+        float b=sc.nextFloat();
+        String str1=sc.next();
+        Scanner n=new Scanner(System.in);
+        String str2=n.nextLine();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+    
+}
